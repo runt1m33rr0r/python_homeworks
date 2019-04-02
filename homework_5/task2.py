24b19bb5353a1d55f32db6baf486ad323371fc44
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-04-02 20:13:09 +0300

    fixed some bugs

--- homework_5/task2.py
+++ homework_5/task2.py
@@ -8,36 +8,36 @@ class Game:
         self.padding = 10
         self.words = self.load_words()
         self.selected_word = self.select_word()
-        self.guessed = [self.selected_word[0], self.selected_word[len(self.selected_word) - 1]]
+        self.guessed = []
         self.intial_tries = 6
         self.tries = self.intial_tries
-
+        
         self.app = wx.App()
         self.frame = wx.Frame(None, title='game', size=(600, 700))
         self.panel = wx.Panel(self.frame)
 
         self.font = wx.Font(20, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-        self.player_message = wx.StaticText(self.panel, label='')
+        self.player_message = wx.StaticText(self.panel, label=' ')
         self.player_message.SetFont(self.font)
         self.word = wx.StaticText(self.panel, label=self.construct_word())
         self.word.SetFont(self.font)
+
+        self.letter_buttons = []
         self.letters_grid = wx.GridSizer(rows=3, cols=11, hgap=0, vgap=0)
+        for let in string.ascii_uppercase:
+            button = wx.ToggleButton(self.panel, label=let, size=(50, 50))
+            button.Bind(wx.EVT_TOGGLEBUTTON, self.select_letter(button.GetLabelText()))
+            self.letters_grid.Add(button)
+            self.letter_buttons.append(button)
 
         self.content = wx.BoxSizer(wx.VERTICAL)
         self.content.AddSpacer(self.padding)
         self.content.Add(self.word, flag=wx.ALIGN_CENTER)
         self.content.AddSpacer(self.padding)
-        self.content.Add(self.player_message)
+        self.content.Add(self.player_message, flag=wx.ALIGN_CENTER)
         self.content.AddSpacer(self.padding)
         self.content.Add(self.letters_grid, flag=wx.ALIGN_CENTER)
         self.content.AddSpacer(self.padding)
-        self.letter_buttons = []
-
-        for let in string.ascii_uppercase:
-            button = wx.ToggleButton(self.panel, label=let, size=(50, 50))
-            button.Bind(wx.EVT_TOGGLEBUTTON, self.select_letter(button.GetLabel()))
-            self.letters_grid.Add(button)
-            self.letter_buttons.append(button)
 
         self.images = self.load_images()
         self.image = wx.StaticBitmap(self.panel, bitmap=self.images[0])
@@ -56,14 +56,12 @@ class Game:
 
     
     def reset_game(self, ev):
-        for button in self.letter_buttons:
-            button.SetValue(False)
-
+        self.toggle_buttons(False)
         self.player_message.SetLabel('')
         self.tries = self.intial_tries
         self.selected_word = self.select_word()
-        self.guessed = [self.selected_word[0], self.selected_word[len(self.selected_word) - 1]]
-        self.word.SetLabel(self.construct_word())
+        self.guessed = []
+        self.word.LabelText = self.construct_word()
         self.image.SetBitmap(self.images[0])
 
 
@@ -86,36 +84,50 @@ class Game:
     def construct_word(self):
         result = []
         
-        for let in self.selected_word:
-            if let in self.guessed:
-                result.append(let)
+        result.append(self.selected_word[0])
+        for i in range(1, len(self.selected_word) - 1):
+            if self.selected_word[i] in self.guessed:
+                result.append(self.selected_word[i])
             else:
                 result.append('_')
+        result.append(self.selected_word[len(self.selected_word) - 1])
 
         return ''.join(result)
 
 
     def control_game_state(self):
+        if self.tries <= 0:
+            return
+
         for let in self.guessed:
             if let not in self.selected_word:
                 self.tries -= 1
                 self.guessed.remove(let)
                 self.image.SetBitmap(self.images[self.intial_tries - self.tries])
 
+        self.word.SetLabel(self.construct_word())
         if '_' not in self.word.GetLabel():
             self.player_message.SetLabel('You win!')
         elif self.tries == self.intial_tries:
             self.player_message.SetLabel('')
         elif self.tries <= 0:
             self.player_message.SetLabel('You lose!')
+            self.toggle_buttons(True)
         else:
             self.player_message.SetLabel('You have %d tries left!' % self.tries)
+
+        self.word.CenterOnParent(dir=wx.HORIZONTAL)
+        self.player_message.CenterOnParent(dir=wx.HORIZONTAL)
+
+
+    def toggle_buttons(self, state):
+        for button in self.letter_buttons:
+            button.SetValue(state)
 
 
     def select_letter(self, letter):
         def on_select(ev):
             self.guessed.append(letter)
-            self.word.SetLabel(self.construct_word())
             self.control_game_state()
 
         return on_select
